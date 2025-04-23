ccb3b42899a05d0561c53ea923ecc3adf37a41d2
Hohuuthien <hohuuthien270598@gmail.com>
2025-04-23 14:37:40 +0700

    new

--- src/main/java/ass/ass/config/ZalopayConfig.java
+++ src/main/java/ass/ass/config/ZalopayConfig.java
@@ -10,9 +10,9 @@ public class ZalopayConfig {
 
     public static final Map<String, String> config = new HashMap<String, String>() {
         {
-            put("app_id", "2554");
-            put("key1", "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn");
-            put("key2", "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf");
+            put("app_id", "553");
+            put("key1", "9phuAOYhan4urywHTh0ndEXiV3pKHr5Q");
+            put("key2", "Iyz2habzyr7AG8SgvoBCbKwKi3UzlLi3");
             put("endpoint", "https://sb-openapi.zalopay.vn/v2/create");
             put("orderstatus", "https://sb-openapi.zalopay.vn/v2/query");
 
--- src/main/java/ass/ass/models/Orders.java
+++ src/main/java/ass/ass/models/Orders.java
@@ -1,6 +1,6 @@
 package ass.ass.models;
 
-import java.sql.Date;
+import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
